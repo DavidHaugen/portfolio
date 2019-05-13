02bcc6ed7e041ce9e3fc363446688bafd6979fc3
David Haugen <haugen.dj@gmail.com>
2019-05-13 11:19:05 -0500

    add MunchStats to projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import AnimalQuiz from './images/animal-quiz.png';
+import MunchStats from './images/munchstats.png';
 import Remindful from './images/Remindful.png';
 import SpacedRepetition from './images/SpacedRepetition.png';
 
@@ -14,8 +14,8 @@ export default class Projects extends Component{
           </div>
           <p className="project-description">Remindful allows users to record and write about their personal goals. On the first of every month, users receive an email encouraging them to check on their goals and write about their progress. It gives users a dedicated space to track what's important to them and provides a little nudge to check in once a month without the stress of constant notifications. Life is busy, but Remindful can help remind users what they care about.</p>
           <p className="tech-used">Technologies used:</p>
-          <p className="tech-used">Client: HTML, CSS, JavaScript, React, React Context API, React Router. Deployed with Zeit Now.</p>
-          <p className="tech-used">Server/Database: Node.js, Express, PostgreSQL, Heroku Scheduler, Nodemailer, Postgrator, Chai, Mocha, Supertest. Deployed using Heroku.</p>
+          <p className="tech-used">Client: HTML, CSS, JavaScript, React, React Context API, React Router, JWT. Deployed with Zeit Now.</p>
+          <p className="tech-used">Server/Database: Node.js, Express, PostgreSQL, JWT, Heroku Scheduler, Nodemailer, Postgrator, Chai, Mocha, Supertest. Deployed using Heroku.</p>
           <div className="project-links">
             <a href="https://github.com/DavidHaugen/remindful-client" target="_blank" rel="noopener noreferrer"><button className="button-border left-button">View repo</button></a>
             <a href="https://remindful.now.sh/" target="_blank" rel="noopener noreferrer"><button className="button-solid right-button">View live</button></a>
@@ -28,24 +28,25 @@ export default class Projects extends Component{
           </div>
           <p className="project-description">The spaced repetition technique helps users learn a new language by focusing on the words that give them the most trouble. This app tracks tracks user answers over time; if users get a word wrong, they are presented with that word more often. As the user masters a word it is presented less and less frequently.</p>
           <p className="tech-used">Technologies used:</p>
-          <p className="tech-used">Client: React, React Context API, React Router, HTML, CSS, JavaScript, Cypress. Deployed with Zeit Now</p>
-          <p className="tech-used">Server: Node.js, Express, PostgreSQL, Postgrator, Chai, Mocha, Supertest. Deployed using Heroku.</p>
+          <p className="tech-used">Client: React, React Context API, React Router, JWT, HTML, CSS, JavaScript, Cypress. Deployed with Zeit Now</p>
+          <p className="tech-used">Server: Node.js, Express, PostgreSQL, Postgrator, JWT, Chai, Mocha, Supertest. Deployed using Heroku.</p>
           <div className="project-links">
             <a href="https://github.com/DavidHaugen/Spaced-repetition-client" target="_blank" rel="noopener noreferrer"><button className="button-border left-button">View repo</button></a>
             <a href="https://spaced-repetition.now.sh/" target="_blank" rel="noopener noreferrer"><button className="button-solid right-button">View live</button></a>
           </div>
         </div>
         <div className="project-container">
-          <h2>Animal quiz</h2>
+          <h2>Munch Stats</h2>
           <div className="image-container">
-            <img src={AnimalQuiz} alt="Project screenshot" className="project-screenshot"/>
+            <img src={MunchStats} alt="Project screenshot" className="project-screenshot"/>
           </div>
           <p className="tech-used">Technologies used:</p>
-          <p className="tech-used">HTML, CSS, JavaScript, jQuery, deployed with Github Pages.</p>
-          <p className="project-description">Animal Facts Quiz is a clean, simple app that allows users to test themselves on unique animal facts. The app tracks user progress, provides feedback, and provides a summary of user results at the end of the quiz. </p>
+          <p className="tech-used">Client: React, React Context API, React Router, JWT, HTML, CSS, JavaScript, Jest, Enzyme, Chart.js. Deployed with Zeit Now.</p>
+          <p className="tech-used">Server: Node.js, Express, PostgreSQL, Postgrator, JWT, Chai, Mocha, Supertest, Travis CI. Deployed using Heroku.</p>
+          <p className="project-description">Munch Stats is a nutrition tracking app with a that allows users to create, edit, and log their own meals. Users can set daily goals for calories, carbohydrates, fat, and protein and track how their daily consumption compares to their own personal goals. Munch Stats makes it easy to track the meals you make regularly, helping you stay healthy and happy.</p>
           <div className="project-links">
-            <a href="https://github.com/DavidHaugen/animal-quiz" target="_blank" rel="noopener noreferrer"><button className="button-border left-button">View repo</button></a>
-            <a href="https://davidhaugen.github.io/animal-quiz/" target="_blank" rel="noopener noreferrer"><button className="button-solid right-button">View live</button></a>
+            <a href="https://github.com/thinkful-ei-armadillo/munchstats-client" target="_blank" rel="noopener noreferrer"><button className="button-border left-button">View repo</button></a>
+            <a href="https://munchstats.now.sh/" target="_blank" rel="noopener noreferrer"><button className="button-solid right-button">View live</button></a>
           </div>
         </div>
       </section>
